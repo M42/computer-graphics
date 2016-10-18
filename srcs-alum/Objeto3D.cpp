8f555c86eb07fe16bddc322d83bac397b66c13cc
M42 <mromang08@gmail.com>
2016-10-18 11:08:07 +0200

    Implementa Objeto3D

---
+++ srcs-alum/Objeto3D.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include "practicas.hpp"
+#include "Objeto3D.hpp"
+
+// Implementa la función que devuelve el nombre y que puede usarse
+// para depurar.
+std::string Objeto3D::nombre() {
+    return nombre_objeto;
+}
+
+
--- srcs-alum/Objeto3D.hpp
+++ srcs-alum/Objeto3D.hpp
@@ -1,10 +1,19 @@
-#include<string>
+#ifndef OBJETO3D
+#define OBJETO3D
+
+#include <string>
 #include "practicas.hpp"
 
 class Objeto3D {
 protected:
+  // Nombre interno del objeto. Puede usarse para depurar.
   std::string nombre_objeto;
 public:
+  // Devuelve el nombre interno del objeto.
   std::string nombre();
+
+  // Visualiza un objeto con el modo de visualización dado.
   virtual void visualizarGL(ContextoVis& cv) = 0;
 };
+
+#endif
