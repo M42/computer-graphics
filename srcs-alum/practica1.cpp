926fa615fec0eb0e91f30b31daca8c02e901012f
M42 <mromang08@gmail.com>
2016-10-20 20:36:10 +0200

    Cubo y tetraedro

--- srcs-alum/practica1.cpp
+++ srcs-alum/practica1.cpp
@@ -17,7 +17,59 @@ unsigned objeto_activo = 0 ; // objeto activo: cubo (0), tetraedro (1), otros...
 
 // ---------------------------------------------------------------------
 // declaraciones del cubo y el tetraedro
+Cubo::Cubo() {
+  // Nombra el objeto
+  nombre_objeto = "Cubo creado";
 
+  // Añade los vértices al cubo
+  // TODO: Reescribir en binario
+  vertices.push_back(Tupla3i(0,0,0));
+  vertices.push_back(Tupla3i(0,0,1));
+  vertices.push_back(Tupla3i(0,1,0));
+  vertices.push_back(Tupla3i(0,1,1));
+  vertices.push_back(Tupla3i(1,0,0));
+  vertices.push_back(Tupla3i(1,0,1));
+  vertices.push_back(Tupla3i(1,1,0));
+  vertices.push_back(Tupla3i(1,1,1));
+
+  // Añade las caras del cubo
+  // x = 0
+  caras.push_back(Tupla3i(0,1,3));
+  caras.push_back(Tupla3i(0,2,3));
+  // x = 1
+  caras.push_back(Tupla3i(4,5,7));
+  caras.push_back(Tupla3i(4,6,7));
+  // y = 0
+  caras.push_back(Tupla3i(0,1,5));
+  caras.push_back(Tupla3i(0,4,5));
+  // y = 1
+  caras.push_back(Tupla3i(2,3,7));
+  caras.push_back(Tupla3i(2,6,7));
+  // z = 0
+  caras.push_back(Tupla3i(0,2,6));
+  caras.push_back(Tupla3i(0,4,6));
+  // z = 1
+  caras.push_back(Tupla3i(1,3,7));
+  caras.push_back(Tupla3i(1,5,7));
+};
+
+Tetraedro::Tetraedro() {
+  // Nombra el objeto
+  nombre_objeto = "Tetraedro creado";
+
+  // Añade los vértices del tetraedro
+  // Es un tetraedro inscrito en un cubo con lado 2*sqrt(2)
+  vertices.push_back(Tupla3i(+1,+1,+1));
+  vertices.push_back(Tupla3i(-1,-1,+1));
+  vertices.push_back(Tupla3i(-1,+1,-1));
+  vertices.push_back(Tupla3i(+1,-1,-1));
+
+  // Caras del tetraedro
+  caras.push_back(Tupla3i(0,1,2));
+  caras.push_back(Tupla3i(0,1,3));
+  caras.push_back(Tupla3i(0,2,3));
+  caras.push_back(Tupla3i(1,2,3));
+};
 
 // ---------------------------------------------------------------------
 // Función para implementar en la práctica 1 para inicialización.
